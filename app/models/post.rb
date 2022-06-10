5dac5e43932d577b6dd34caf010a6bb25148cf45
nemoto.yojiro <nemoto.yojiro@Donuts-NM2069-2201.local>
2022-06-10 13:00:15 +0900

    add regulation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 10 }, uniqueness: { case_sensitive: false }
+  validates :content,  presence: true, length: { maximum: 100 }
 end
